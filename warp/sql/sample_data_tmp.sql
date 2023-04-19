f497b858e8169eb24be212eee863abc0060d478e
powerspt <45279624+powerspt@users.noreply.github.com>
2023-04-19 09:56:10 +0900

    test

--- warp/sql/sample_data_tmp.sql
+++ warp/sql/sample_data_tmp.sql
@@ -3,25 +3,32 @@ INSERT INTO users VALUES ('user1','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65
 INSERT INTO users VALUES ('user2','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Bar',20);
 INSERT INTO users VALUES ('user3','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Baz',20);
 
-INSERT INTO zone (id,zone_group,name) VALUES (1,1,'Zone 1A');
-INSERT INTO zone (id,zone_group,name) VALUES (2,1,'Zone 1B');
-INSERT INTO zone (id,zone_group,name) VALUES (3,2,'Parking');
+INSERT INTO zone (id,zone_group,name) VALUES (1,1,'1F');
+INSERT INTO zone (id,zone_group,name) VALUES (2,1,'2F');
+INSERT INTO zone (id,zone_group,name) VALUES (3,2,'3F');
+INSERT INTO zone (id,zone_group,name) VALUES (3,2,'4F');
+INSERT INTO zone (id,zone_group,name) VALUES (3,2,'5F');
 
 -- fix PK sequence
 SELECT pg_catalog.setval(pg_get_serial_sequence('zone', 'id'), (SELECT MAX(id) FROM zone));
 
-INSERT INTO users VALUES ('group_1a',NULL,'Group 1A',100);
-INSERT INTO users VALUES ('group_1b',NULL,'Group 1B',100);
-INSERT INTO users VALUES ('group_parking',NULL,'Group Parking',100);
+INSERT INTO users VALUES ('group_1f',NULL,'Group 1f',100);
+INSERT INTO users VALUES ('group_2f',NULL,'Group 2f',100);
+INSERT INTO users VALUES ('group_3f',NULL,'Group 3f',100);
+INSERT INTO users VALUES ('group_4f',NULL,'Group 4f',100);
+INSERT INTO users VALUES ('group_5f',NULL,'Group 5f',100);
 
---INSERT INTO groups VALUES ('group_1a','user1');
-INSERT INTO groups VALUES ('group_1b','user1');
-INSERT INTO groups VALUES ('group_1a','user2');
+--INSERT INTO groups VALUES ('group_1f','user1');
+INSERT INTO groups VALUES ('group_2f','user1');
+INSERT INTO groups VALUES ('group_1f','user2');
 
 INSERT INTO zone_assign VALUES (1,'user1',10);
-INSERT INTO zone_assign VALUES (1,'group_1a',20);
-INSERT INTO zone_assign VALUES (2,'group_1b',20);
-INSERT INTO zone_assign VALUES (3,'group_parking',30);
+INSERT INTO zone_assign VALUES (1,'group_1f',20);
+INSERT INTO zone_assign VALUES (2,'group_2f',20);
+INSERT INTO zone_assign VALUES (3,'group_3f',30);
+INSERT INTO zone_assign VALUES (3,'group_4f',30);
+INSERT INTO zone_assign VALUES (3,'group_4f',30);
+
 INSERT INTO zone_assign VALUES (1,'admin',10);
 INSERT INTO zone_assign VALUES (2,'admin',10);
 INSERT INTO zone_assign VALUES (3,'admin',10);
