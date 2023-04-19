fd91b5fc35b0a9ae8ce319a4157a3e528aa9a2e5
powerspt <45279624+powerspt@users.noreply.github.com>
2023-04-19 12:22:27 +0900

    sql query fix

--- warp/sql/sample_data_old.sql
+++ warp/sql/sample_data_old.sql
@@ -6,8 +6,6 @@ INSERT INTO users VALUES ('user3','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65
 INSERT INTO zone (id,zone_group,name) VALUES (1,1,'Zone 1A');
 INSERT INTO zone (id,zone_group,name) VALUES (2,1,'Zone 1B');
 INSERT INTO zone (id,zone_group,name) VALUES (3,2,'Parking');
-INSERT INTO zone (id,zone_group,name) VALUES (4,3,'Zone 2A');
-INSERT INTO zone (id,zone_group,name) VALUES (5,3,'Zone 2B');
 
 -- fix PK sequence
 SELECT pg_catalog.setval(pg_get_serial_sequence('zone', 'id'), (SELECT MAX(id) FROM zone));
@@ -15,8 +13,6 @@ SELECT pg_catalog.setval(pg_get_serial_sequence('zone', 'id'), (SELECT MAX(id) F
 INSERT INTO users VALUES ('group_1a',NULL,'Group 1A',100);
 INSERT INTO users VALUES ('group_1b',NULL,'Group 1B',100);
 INSERT INTO users VALUES ('group_parking',NULL,'Group Parking',100);
-INSERT INTO users VALUES ('group_2a',NULL,'Group 2a',100);
-INSERT INTO users VALUES ('group_2b',NULL,'Group 2b',100);
 
 --INSERT INTO groups VALUES ('group_1a','user1');
 INSERT INTO groups VALUES ('group_1b','user1');
@@ -29,8 +25,6 @@ INSERT INTO zone_assign VALUES (3,'group_parking',30);
 INSERT INTO zone_assign VALUES (1,'admin',10);
 INSERT INTO zone_assign VALUES (2,'admin',10);
 INSERT INTO zone_assign VALUES (3,'admin',10);
-INSERT INTO zone_assign VALUES (4,'admin',10);
-INSERT INTO zone_assign VALUES (5,'admin',10);
 
 INSERT INTO seat (zid,name,x,y) VALUES (1,'1.1',22,94);
 INSERT INTO seat (zid,name,x,y) VALUES (1,'1.2',84,85);
@@ -118,5 +112,3 @@ SELECT pg_catalog.setval(pg_get_serial_sequence('blobs', 'id'), (SELECT MAX(id)+
 UPDATE zone SET iid = 1 WHERE id = 1;
 UPDATE zone SET iid = 2 WHERE id = 2;
 UPDATE zone SET iid = 3 WHERE id = 3;
-UPDATE zone SET iid = 4 WHERE id = 4;
-UPDATE zone SET iid = 5 WHERE id = 5;
--- warp/sql/sample_data_tmp.sql
+++ warp/sql/sample_data_tmp.sql
@@ -3,35 +3,34 @@ INSERT INTO users VALUES ('user1','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65
 INSERT INTO users VALUES ('user2','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Bar',20);
 INSERT INTO users VALUES ('user3','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Baz',20);
 
-INSERT INTO zone (id,zone_group,name) VALUES (1,1,'1F');
-INSERT INTO zone (id,zone_group,name) VALUES (2,1,'2F');
-INSERT INTO zone (id,zone_group,name) VALUES (3,2,'3F');
-INSERT INTO zone (id,zone_group,name) VALUES (3,2,'4F');
-INSERT INTO zone (id,zone_group,name) VALUES (3,2,'5F');
+INSERT INTO zone (id,zone_group,name) VALUES (1,1,'Zone 1A');
+INSERT INTO zone (id,zone_group,name) VALUES (2,1,'Zone 1B');
+INSERT INTO zone (id,zone_group,name) VALUES (3,2,'Parking');
+INSERT INTO zone (id,zone_group,name) VALUES (4,3,'Zone 2A');
+INSERT INTO zone (id,zone_group,name) VALUES (5,3,'Zone 2B');
 
 -- fix PK sequence
 SELECT pg_catalog.setval(pg_get_serial_sequence('zone', 'id'), (SELECT MAX(id) FROM zone));
 
-INSERT INTO users VALUES ('group_1f',NULL,'Group 1f',100);
-INSERT INTO users VALUES ('group_2f',NULL,'Group 2f',100);
-INSERT INTO users VALUES ('group_3f',NULL,'Group 3f',100);
-INSERT INTO users VALUES ('group_4f',NULL,'Group 4f',100);
-INSERT INTO users VALUES ('group_5f',NULL,'Group 5f',100);
+INSERT INTO users VALUES ('group_1a',NULL,'Group 1A',100);
+INSERT INTO users VALUES ('group_1b',NULL,'Group 1B',100);
+INSERT INTO users VALUES ('group_parking',NULL,'Group Parking',100);
+INSERT INTO users VALUES ('group_2a',NULL,'Group 2a',100);
+INSERT INTO users VALUES ('group_2b',NULL,'Group 2b',100);
 
---INSERT INTO groups VALUES ('group_1f','user1');
-INSERT INTO groups VALUES ('group_2f','user1');
-INSERT INTO groups VALUES ('group_1f','user2');
+--INSERT INTO groups VALUES ('group_1a','user1');
+INSERT INTO groups VALUES ('group_1b','user1');
+INSERT INTO groups VALUES ('group_1a','user2');
 
 INSERT INTO zone_assign VALUES (1,'user1',10);
-INSERT INTO zone_assign VALUES (1,'group_1f',20);
-INSERT INTO zone_assign VALUES (2,'group_2f',20);
-INSERT INTO zone_assign VALUES (3,'group_3f',30);
-INSERT INTO zone_assign VALUES (3,'group_4f',30);
-INSERT INTO zone_assign VALUES (3,'group_4f',30);
-
+INSERT INTO zone_assign VALUES (1,'group_1a',20);
+INSERT INTO zone_assign VALUES (2,'group_1b',20);
+INSERT INTO zone_assign VALUES (3,'group_parking',30);
 INSERT INTO zone_assign VALUES (1,'admin',10);
 INSERT INTO zone_assign VALUES (2,'admin',10);
 INSERT INTO zone_assign VALUES (3,'admin',10);
+INSERT INTO zone_assign VALUES (4,'admin',10);
+INSERT INTO zone_assign VALUES (5,'admin',10);
 
 INSERT INTO seat (zid,name,x,y) VALUES (1,'1.1',22,94);
 INSERT INTO seat (zid,name,x,y) VALUES (1,'1.2',84,85);
@@ -119,3 +118,5 @@ SELECT pg_catalog.setval(pg_get_serial_sequence('blobs', 'id'), (SELECT MAX(id)+
 UPDATE zone SET iid = 1 WHERE id = 1;
 UPDATE zone SET iid = 2 WHERE id = 2;
 UPDATE zone SET iid = 3 WHERE id = 3;
+UPDATE zone SET iid = 4 WHERE id = 4;
+UPDATE zone SET iid = 5 WHERE id = 5;
